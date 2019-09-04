8da88e84176be9809ea2732909acca7e50fee404
qingpengtan <tang1994year163.com>
2019-09-04 16:35:33 +0800

    test
    Signed-off-by: qingpengtan <tang1994year163.com>

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+    <request-loding></request-loding>
+  </div>
+</template>
+
+<script>
+import RequestLoding from "@/components/RequestLoading.vue";
+export default {
+  components: {
+    RequestLoding
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
